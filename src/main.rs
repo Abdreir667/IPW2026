f12562a02a2a16d0684e1622054252fb22a22bd2
Eva Cosma <eva.cosma.mail@gmail.com>
2026-08-03 12:13:17 +0300

    chore: ipw2026 version
    Signed-off-by: Eva Cosma <eva.cosma.mail@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let peripherals = embassy_stm32::init(Default::default());
 
-    info!("Hello, embassy-rs!");
-    loop {}
+    let mut x = 1;
+    loop {
+        info!("Hello, embassy-rs! x = {}", x);
+        x += 1;
+        Timer::after(Duration::from_millis(500)).await;
+    }
 }
